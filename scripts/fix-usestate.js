4fdee48a6ac6df4608fca5d565f3bfd9d2cd81dd
SavioMohan1 <saviomohan2002@gmail.com>
2026-05-14 17:12:08 +0530

    "cleanup-temp-scripts"

--- scripts/fix-usestate.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = 'src/app/jobs/create/page.tsx';
-let content = fs.readFileSync(path, 'utf8');
-content = content.replace('useState(() => {', 'useEffect(() => {');
-content = content.replace(/checkVerification\(\)\r?\n    \}\)/, 'checkVerification()\n    }, [])');
-fs.writeFileSync(path, content);
-console.log('Done');
